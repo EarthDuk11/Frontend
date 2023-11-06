a3a0e0a14a213276d01a93d4ef8cbefe739c9e39
soyoon1 <20210841@duksung.ac.kr>
2023-11-06 19:57:41 +0900

    펀딩 페이지 xml 초안

--- app/src/main/java/com/example/team11/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/team11/BottomNavigationActivity.kt
@@ -2,12 +2,10 @@ package com.example.team11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.team11.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class BottomNavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_bottom_navigation)
     }
 }
---
+++ app/src/main/java/com/example/team11/MyFundingAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.team11
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.team11.databinding.ItemFundingBinding
+
+
+class MyFundingViewHolder(val binding: ItemFundingBinding) : RecyclerView.ViewHolder(binding.root)
+class MyFundingAdapter (val context: Context, val itemList: MutableList<ItemFundingModel>): RecyclerView.Adapter<MyFundingViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFundingViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return MyFundingViewHolder(ItemFundingBinding.inflate(layoutInflater))
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: MyFundingViewHolder, position: Int) {
+        val data = itemList.get(position)
+
+        holder.binding.run{
+            fundingTitleView.text=data.title
+            fundingOneLineView.text=data.oneLine
+        }
+
+        //스토리지 이미지 다운로드........................ 이미지를 넣어줘야 함.
+//        val imageRef = MyApplication.storage.reference.child("images/${data.docId}.jpg")
+//        imageRef.downloadUrl.addOnCompleteListener{task ->
+//            if(task.isSuccessful){
+//                // 다운로드 이미지를 ImageView에 보여줌.
+//
+//                GlideApp.with(context)
+//                    .load(task.result)
+//                    .into(holder.binding.itemImageView)
+//
+//            }
+//        }
+
+    }
+}
