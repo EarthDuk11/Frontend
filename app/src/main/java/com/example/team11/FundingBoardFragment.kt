cdee06cf243891313930635cc7d7afcb51d57cd7
soyoon1 <20210841@duksung.ac.kr>
2023-11-08 02:26:16 +0900

    폼 링크 AlertDialog 생성 및 버튼 연결

--- app/src/main/java/com/example/team11/FundingBoardFragment.kt
+++ app/src/main/java/com/example/team11/FundingBoardFragment.kt
@@ -1,5 +1,6 @@
 package com.example.team11
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -39,6 +40,10 @@ class FundingBoardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentFundingBoardBinding.inflate(inflater, container, false)
+        binding.fundingWriteButton.setOnClickListener {
+            val intent = Intent(requireContext(), FundingWriteActivity::class.java)
+            startActivity(intent)
+        }
         return binding.root
     }
 
