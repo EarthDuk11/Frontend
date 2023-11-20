95a8a1b98cdfc5711c6cc7903524ee04cde4cdcf
soyoon1 <20210841@duksung.ac.kr>
2023-11-20 20:29:00 +0900

    파일 수정

--- app/src/main/java/com/example/team11/MyApplication.kt
+++ app/src/main/java/com/example/team11/MyApplication.kt
@@ -8,10 +8,10 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 
+
 class MyApplication : MultiDexApplication(){
     companion object{
         lateinit var db : FirebaseFirestore
-        lateinit var storage : FirebaseStorage
 
         lateinit var auth : FirebaseAuth
         lateinit var storage: FirebaseStorage // for diaryActivity
@@ -33,8 +33,6 @@ class MyApplication : MultiDexApplication(){
         auth = Firebase.auth
 
         db = FirebaseFirestore.getInstance() // 하나의 컬렉션을 만들겠다.
-
-        storage = Firebase.storage
-
+        storage = Firebase.storage // for diary image
     }
 }
