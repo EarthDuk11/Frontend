3f8658b8f92417f1f0128261861c47ba9eb49abf
hyeonjichoi21 <ch5i_hj15@naver.com>
2023-11-21 23:43:07 +0900

    로그인/회원가입 레이아웃 + 페이지 이동

--- app/src/main/java/com/example/team11/LoginActivity.kt
+++ app/src/main/java/com/example/team11/LoginActivity.kt
@@ -1,4 +1,21 @@
 package com.example.team11
 
-class LoginActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val registerButton: TextView = findViewById(R.id.registerButton)
+
+        // 회원가입 버튼 클릭 시 RealSigninActivity로 이동
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RealSigninActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
